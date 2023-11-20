a9ed967873b8d431babf30ecabb172bc6299e54f
LuciaOcana <lucia.ocana@estudiantat.upc.edu>
2023-11-20 16:02:09 +0100

    Interfaz grafica V1.1.0

--- ListApp/app/src/main/java/dsa/upc/edu/dsa_project_android/Manager/Manager.java
+++ ListApp/app/src/main/java/dsa/upc/edu/dsa_project_android/Manager/Manager.java
@@ -1,4 +1,5 @@
-package com.example.dsa_project_android.Manager;
+package dsa.upc.edu.dsa_project_android.Manager;
+
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -12,25 +13,19 @@ import retrofit2.http.Path;
 
 public interface Manager {
 
-    String URL = "http://10.0.2.2:8080/dsaApp/";
+    String URL = "http://10.0.2.2:8080/dsaApp/pixelRush/";
 
     //----------------------------------------------------------------------------------------------
 
     //Posibles acciones realizables
-    @GET("/dsaApp/tracks")
-    Call<List<Song>> songs();
+    @GET("getObjectListFromStore")
+    Call<List<StoreObject>> objects();
 
-    @GET("/dsaApp/tracks/{id}")
-    Call<List<Song>> songsID(@Path("id") String varId);
+    @POST("registerNewUser")
+    Call<Void> newUser(@Body User newUser);
 
-    @DELETE("/dsaApp/tracks/{id}")
-    Call<Void> responseDelete(@Path("id") String varIdDel);
-
-    @POST("/dsaApp/tracks")
-    Call<Song> createSong(@Body Song varSongCreated);
-
-    @PUT("/dsaApp/tracks")
-    Call<Void> updateSong(@Body Song varSongCreated);
+    @POST("login")
+    Call<Boolean> user(@Body String username,@Body String password);
 
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(URL)
