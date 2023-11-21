6cb45694e56c44a584a16396ead8002f84b5d7e8
intxz <angel.redondo@estudiantat.upc.edu>
2023-11-21 12:34:25 +0100

    para roberto

--- app/src/main/java/com/example/dsa_project_android/Manager/Manager.java
+++ app/src/main/java/com/example/dsa_project_android/Manager/Manager.java
@@ -1,7 +1,7 @@
 package com.example.dsa_project_android.Manager;
 
 import retrofit2.Call;
-import retrofit2.http.Field;
+import retrofit2.http.Body;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
@@ -9,12 +9,9 @@ public interface Manager {
 
     @FormUrlEncoded
     @POST("registerNewUser")
-    Call<Void> register(
-            @Field("username") String username,
-            @Field("password") String password,
-            @Field("name") String name,
-            @Field("surname") String surname,
-            @Field("mail") String mail,
-            @Field("age") int age
-    );
+    Call<Void> register(@Body RegisterCredentials credentials);
+
+    @FormUrlEncoded
+    @POST("login")
+    Call<Void> login(@Body LoginCredentials credentials);
 }
